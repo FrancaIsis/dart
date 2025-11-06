b3256323d2040c668b28fe53996a5dc913f98119
FrancaIsis <isisdej@outlokk.com>
2025-11-05 21:22:45 -0300

    .

---
+++ material/maps/exemplo01.dart
@@ -0,0 +1,33 @@
+void main() {
+  //Map de agenda: cada chave é o nome da pessoa
+  //e cada valor é outro Map com informações detalhadas (idade e email)
+  Map<String, dynamic> agenda = {
+    'John': {'idade': 50, 'email': 'john@gmail.com'},
+    'Jane': {'idade': 40, 'email': 'jane@gmail.com'},
+  };
+
+  //
+  //Map de cadastro pessoal: chave -> valor com tipos variados (int, double, String)
+  //dynamic permite que cada valor seja de tipo diferente
+
+  Map<String, dynamic> cadastro = {
+    'nome':'John Doe',
+    'nascimetno': 1970;
+    'altura': 1.80,
+    'est_civil':'solteiro';
+    'cidade':'Juiz de Fora',
+    'uf':'MG'
+  };
+
+  //
+  //Saída formatada
+  print('-'*70);
+  print('Minha agenda');
+  print(agenda);// imprime o map completo
+  print('-'*70);
+
+  print('-'*70);
+  print('Minha cadastro');
+  print(cadastro);// imprime o map completo
+  print('-'*70);
+}
